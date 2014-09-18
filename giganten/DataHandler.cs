4998a1b2c3691642e24add054beedcff80d9fc45
Armienn <nigathr@gmail.com>
2014-09-18 13:39:53 +0200

    Date reading

--- giganten/DataHandler.cs
+++ giganten/DataHandler.cs
@@ -7,6 +7,8 @@ using Nea;
 
 namespace giganten {
 	class DataHandler {
+		private static const int fieldcount = 20;
+		private static const int datefield = 2;
 		List<String> files = new List<string>();
 		List<YearInfo> years = new List<YearInfo>();
 
@@ -24,16 +26,27 @@ namespace giganten {
 		private void ProcessFile(string file) {
 			NeaReader reader = new NeaReader(file);
 			while (reader.Peek() != -1) {
-				ProcessEntry(reader, 4);
+				ProcessEntry(reader);
 			}
 			reader.Close();
 		}
 
-		private void ProcessEntry(NeaReader reader, int fieldcount) {
+		private void ProcessEntry(NeaReader reader) {
 			String[] fields = new String[fieldcount];
 			for (int i = 0; i < fieldcount; i++) {
 				fields[i] = reader.ReadUntilAny(";,");
 				if (reader.Peek() == -1) return;
+			}
+			int month;
+			int year;
+			try {
+				String smonth = fields[datefield].Substring(3, 2);
+				String syear = fields[datefield].Substring(6, 2);
+				month = int.Parse(smonth);
+				year = int.Parse(syear);
+			}
+			catch (Exception e) {
+				
 			}
 		}
 	}
