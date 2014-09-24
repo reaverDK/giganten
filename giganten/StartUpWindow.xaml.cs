2c2be8b01fa8f0a62226ef9e89dbcc1a52f9f778
Armienn <nigathr@gmail.com>
2014-09-24 15:33:28 +0200

    Opstartsloading startet

--- giganten/StartUpWindow.xaml.cs
+++ giganten/StartUpWindow.xaml.cs
@@ -11,14 +11,51 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.IO;
+using System.Threading;
 
 namespace giganten {
 	/// <summary>
 	/// Interaction logic for StartUpWindow.xaml
 	/// </summary>
 	public partial class StartUpWindow : Window {
+		String fileToLoad = null;
+
 		public StartUpWindow() {
 			InitializeComponent();
+		}
+
+		private void LoadDefaultFiles() {
+			String[] filePaths = null;
+			try {
+				filePaths = Directory.GetFiles(@"\rankingdata\", "*.csv");
+			}
+			catch (Exception e) {
+				
+			}
+			if(filePaths != null)
+				if (filePaths.Length == 1) {
+					fileToLoad = filePaths[0];
+				}
+			
+			if (fileToLoad == null) {
+				SetText(StatusLabel, "Ingen fil fundet");
+			}
+			else {
+				SetText(StatusLabel, "Loader filen: " + fileToLoad);
+			}
+		}
+
+		delegate void SetTextCallback(Label control, string text);
+
+		private void SetText(Label control, string text) {
+			if (control.Dispatcher.CheckAccess()) {
+				control.Content = text;
+			}
+			else {
+				SetTextCallback d = new SetTextCallback(SetText);
+				control.Dispatcher.Invoke(d, new object[] { control, text });
+			}
 		}
 
 		private void Grid_MouseDown(object sender, MouseButtonEventArgs e) {
@@ -35,6 +72,12 @@ namespace giganten {
 			MainWindow main = new MainWindow();
 			main.Show();
 			this.Close();
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e) {
+			StatusLabel.Content = "Ser efter filer";
+			Thread thread = new Thread(LoadDefaultFiles);
+			thread.Start();
 		}
 	}
 }
