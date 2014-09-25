75fe121cf7e31f9c7041ee6969a7c07549a13122
Armienn <nigathr@gmail.com>
2014-09-25 19:26:43 +0200

    Added Salesman class

---
+++ giganten/DataHandler/MonthInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace giganten {
+	public class MonthInfo {
+		List<Salesman> Salesmen = new List<Salesman>();
+
+		public MonthInfo(String[] entry) {
+			Salesmen.Add(new Salesman(entry));
+		}
+
+		public void AddEntry(String[] entry) {
+			Salesman man = GetSalesman(entry[3]);
+			if (man != null) {
+				man.AddEntry(entry);
+			}
+			else {
+				Salesmen.Add(new Salesman(entry));
+			}
+		}
+
+		public Salesman GetSalesman(String name) {
+			foreach (Salesman sm in Salesmen) {
+				if (sm.Name == name)
+					return sm;
+			}
+			return null;
+		}
+	}
+}
---
+++ giganten/DataHandler/Salesman.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//0: Afd
+//1: BilagsNr
+//2: Dato
+//3: SK
+//4: Varenummer
+//5: Beskrivelse
+//6: Antal
+//7: Salgspris
+//8: KostPris
+//9: Bto.%
+//10: Bto.kr.
+//11: Rab%
+//12: KGM
+//13: M‘rke
+//14: MVA%
+//15: Kundenummer
+//16: Kundetype
+//17: Uge
+//18: EksternNr
+//19: Serienr.
+
+namespace giganten {
+	public class Salesman {
+		public readonly String Name;
+		public Salesman(String[] entry) {
+			Name = entry[3];
+			AddEntry(entry);
+		}
+
+		public void AddEntry(String[] entry) {
+
+		}
+	}
+}
